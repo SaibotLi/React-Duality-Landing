fdec9de2dddd055a6537caf674e4b6dbd6e3a122
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-03 20:20:09 -0300

    NavBar looks beautifulll!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
 <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
 <div className="container px-4 mx-auto relative text-sm">
-    <div className="flex justify-center items-center">
+    <div className="flex justify-between items-center">
         <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2"src={logo} alt="" />
             <span className="text-xl tracking-tight">Duality</span>
@@ -16,10 +16,16 @@ const Navbar = () => {
         <a href={item.href}> {item.label}
         </a>
     </li>
-    )
-)
-}
+    ))}
 </ul>
+<div className="hidden lg:flex justify-center space-x-12 items-center">
+    <a href="#" className="py-2 px-3 border rounder-md">
+        Iniciar sesión
+    </a>
+    <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md">
+Registrarse
+    </a>
+</div>
     </div>
 </div>
 </nav>
