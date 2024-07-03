65b579402ccb1cd349d3119e245bfd8506995003
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-03 20:35:26 -0300

    NavBar prototype ready for Mobile and LG screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
+import {Menu, X} from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
-import { navItems } from "../constants"
+import { navItems } from "../constants";
 
 const Navbar = () => {
+{/* State for Mobile Menu */}
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+    const toggleNavBar = () => {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    };
+
   return (
 <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
 <div className="container px-4 mx-auto relative text-sm">
     <div className="flex justify-between items-center">
+
+        {/* Logo */}
         <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2"src={logo} alt="" />
             <span className="text-xl tracking-tight">Duality</span>
         </div>
+
+        {/* Handle items from ./constants*/}
         <ul className="hidden lg:flex ml-14 space-x-12">
 {navItems.map((item, index) => (
     <li key={index}>
@@ -18,6 +30,8 @@ const Navbar = () => {
     </li>
     ))}
 </ul>
+
+{/* User */}
 <div className="hidden lg:flex justify-center space-x-12 items-center">
     <a href="#" className="py-2 px-3 border rounder-md">
         Iniciar sesión
@@ -26,7 +40,35 @@ const Navbar = () => {
 Registrarse
     </a>
 </div>
+
+{/* Adds mobile menu button */}
+<div className="lg:hidden md:flex flex-col justify-end">
+    <button onClick={toggleNavBar}>
+        {mobileDrawerOpen ? <X /> : <Menu />}
+    </button>
+</div>
+</div>
+
+{/* Adds mobile items */}
+{mobileDrawerOpen && (
+    <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+        <ul>
+            {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                    <a href={item.href}>{item.label}</a>
+                </li>
+            ))}
+        </ul>
+        <div className="flex space-x-6">
+            <a href="#" className="py-2 px-3 border rounded-md">
+                Iniciar sesión
+            </a>
+            <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">
+                Registrarse
+            </a>
+        </div>
     </div>
+)}
 </div>
 </nav>
   )
