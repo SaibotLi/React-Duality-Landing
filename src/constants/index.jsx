8f1d7638bafa29f6c7c145a7541769cc8247e8bc
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-03 20:04:34 -0300

    Navbar halfway through looking great so far!

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -13,10 +13,10 @@ import user5 from "../assets/profile-pictures/user5.jpg";
 import user6 from "../assets/profile-pictures/user6.jpg";
 
 export const navItems = [
-  { label: "Features", href: "#" },
-  { label: "Workflow", href: "#" },
-  { label: "Pricing", href: "#" },
-  { label: "Testimonials", href: "#" },
+  { label: "Inicio", href: "#" },
+  { label: "Productos", href: "#" },
+  { label: "Sobre Nosotros", href: "#" },
+  { label: "Contacto", href: "#" },
 ];
 
 export const testimonials = [
