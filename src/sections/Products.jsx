54297d3df69d5187cba213a17936d2bb075448d6
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-06 11:05:10 -0300

    Product section planning

---
+++ src/sections/Products.jsx
@@ -0,0 +1,23 @@
+const Products = () => {
+  return (
+<div>
+{/*The above div is the Parent-flexbox*/}
+<div> 
+{/* List with different type of products */}
+<ul>
+    <li>Product 1</li>
+    <li>Product 2</li>
+    <li>Product 3</li>
+    <li>Product 4</li>
+    <li>Product 5</li>
+    <li>Product 6</li>
+</ul>
+</div>
+  <div>
+  {/* Render selected product on a grid */}
+  </div>
+  </div>
+  )
+}
+
+export default Products
