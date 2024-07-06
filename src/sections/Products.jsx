8951423893895f3f7b314f8e06fe339cbe207efb
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-06 13:17:35 -0300

    Just planning how Products would look

--- src/sections/Products.jsx
+++ src/sections/Products.jsx
@@ -1,4 +1,4 @@
-import { itemList } from "../components/itemList"
+import itemList from "../components/itemList";
 
 {/*
   - Add functionality to the list of each product, so when the user clicks a product it renders the images
@@ -10,17 +10,26 @@ import { itemList } from "../components/itemList"
 const Products = () => {
   return (
     <section>
- {/* List with different type of products */}
-<div>
-       
-        <ul>
-          {itemList}
-        </ul>
-      </div>
 
-      <div>
-        {/* Render selected product on a grid */}
-      </div>
+ {/* 
+ - Render a list with different type of products, this should beat flex-start displayed on a column, that's why i chose <aside> 'cause it's on the left
+ */}
+
+ <aside>
+  <ul>
+    {itemList}
+  </ul>
+  </aside>
+
+  <div>
+
+{/* 
+- Render selected product on a grid, this should be centered and on a row of 1-2-3-4 products depending size of the screen
+- Maybe this should be a <picture> instead of a <div> and inside i should render the <img> based on a map just as we did with list
+- We used <aside> above because it was aside, what would this be? <main> is already declared at App, maybe it's okay to leave it like <picture> <img /> </picture>
+*/}
+
+  </div>
 
     </section>
   );
