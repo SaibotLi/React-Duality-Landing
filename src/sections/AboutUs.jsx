fea6e3d53e71880bc221064b6435b2e5214a54ea
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-07-11 09:28:10 -0300

    Section AboutUs prototype ready

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -1,9 +1,35 @@
+import { bigShoe1 } from "../assets/images"
 const AboutUs = () => {
   return (
-    <section className="w-full h-screen text-center">
-        <div className="w-full grid grid-rows-2">
-        {/* Add picture to the left, text to the right */}
-        </div>
+    <section className="w-full h-screen">
+        <div className="w-full bg-gray-500 grid grid-rows-2 grid-cols-2">
+          <div className="w-full bg-white">
+          <h1 className="flex justify-start w-2/4 text-black">Sobre nosotros</h1>
+          <p></p>
+          </div>
+          <div className="w-full">
+          <img className='w-2/4 h-[282px] bg-none max-sm:w-[200px] max-sm:h-[200px]' src={bigShoe1} />
+          </div>
+</div>
+<div className="w-full bg-gray-500 grid grid-rows-2 grid-cols-2">
+<div className="w-full">
+          <img className='w-2/4 h-[282px] bg-none max-sm:w-[200px] max-sm:h-[200px]' src={bigShoe1} />
+          </div>
+          <div className="w-full bg-white">
+          <h1 className="flex justify-start w-2/4 text-black">Sobre nosotros</h1>
+          <p></p>
+          </div>
+</div>
+
+<div className="w-full bg-gray-500 grid grid-rows-2 grid-cols-2">
+          <div className="w-full bg-white">
+          <h1 className="flex justify-start w-2/4 text-black">Sobre nosotros</h1>
+          <p></p>
+          </div>
+          <div className="w-full">
+          <img className='w-2/4 h-[282px] bg-none max-sm:w-[200px] max-sm:h-[200px]' src={bigShoe1} />
+          </div>
+</div>
         </section>
   )
 }
