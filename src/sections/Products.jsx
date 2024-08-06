851b55a7e078debf4c9c5aeda23aa6aa49a2b315
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-08-06 13:51:22 -0300

    Fixed the size of /products imgs

--- src/sections/Products.jsx
+++ src/sections/Products.jsx
@@ -23,15 +23,24 @@ const Products = ({ onProductSelect }) => {
         {/* Product images */}
         <div className="flex-1 bg-orange-800 flex justify-center items-start mb-10">
           {selectedProduct && productData[selectedProduct] ? (
-            <div id={selectedProduct.toLowerCase()} className="grid grid-cols-1 md:grid-cols-3 gap-y-4 gap-x-40 max-w-screen-lg mx-auto p-4 h-5/6 place-content-start">
+            <div
+              id={selectedProduct.toLowerCase()}
+              className="grid grid-cols-1 md:grid-cols-3 gap-y-4 gap-x-40 max-w-screen-lg mx-auto p-4 h-5/6 place-content-start"
+            >
               {productData[selectedProduct].map((imgUrl, index) => (
                 <div key={index} className="m-4 flex justify-center">
-                  <img src={imgUrl} alt={selectedProduct} className="object-cover rounded-lg" />
+                  <img
+                    src={imgUrl}
+                    alt={selectedProduct}
+                    className="w-48 h-48 object-cover rounded-lg"
+                  />
                 </div>
               ))}
             </div>
           ) : (
-            <p className="flex items-center h-screen text-white font-semibold">Elige un producto para ver imágenes.</p>
+            <p className="flex items-center h-screen text-white font-semibold">
+              Elige un producto para ver imágenes.
+            </p>
           )}
         </div>
       </section>
