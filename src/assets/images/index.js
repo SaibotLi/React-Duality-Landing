b186cbff8d97d867c32bf64ad11566a1236f2601
SaibotLi <155107204+SaibotLi@users.noreply.github.com>
2024-08-08 20:50:46 -0300

    saving

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,57 +1,46 @@
-import footerLogo from './footer-logo.svg'
-import headerLogo from './header-logo.svg'
+import thumbnailImg1 from "./thumbnail1.png";
+import thumbnailImg2 from "./thumbnail2.jpeg";
+import thumbnailImg3 from "./thumbnail3.png";
+import bigImg1 from "./thumbnail1.png";
+import bigImg2 from "./thumbnail2.jpeg";
+import bigImg3 from "./thumbnail3.png";
 
-import offer from './offer.svg'
-import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailImg1 from './thumbnail1.png'
-import thumbnailImg2 from './thumbnail2.jpeg'
-import thumbnailImg3 from './thumbnail3.png'
-import bigImg1 from "./thumbnail1.png"
-import bigImg2 from "./thumbnail2.jpeg"
-import bigImg3 from "./thumbnail3.png"
-
-import accesorio1 from "./Accesorio1.jpeg"
-import accesorio2 from "./Accesorio2.png"
-import bandera1 from "./Bandera1.png"
-import bandera2 from "./Bandera2.png"
-import mochila1 from "./Mochila1.jpeg"
-import mochila2 from "./Mochila2.jpeg"
-import ploteado1 from "./Ploteado1.png"
-import remera2 from "./Remera2.png"
-import remeraF1 from "./RemeraF1.png"
-import remeraT1 from "./RemeraT1.png"
-import sobreNosotros1 from "./SobreNosotros1.png"
-import taza1 from "./Taza1.jpeg"
-import taza2 from "./Taza2.jpeg"
-import gorra1 from "./Gorras1.jpeg"
-import banner from "./Banner1.png"
+import accesorio1 from "./Accesorio1.jpeg";
+import accesorio2 from "./Accesorio2.png";
+import bandera1 from "./Bandera1.png";
+import bandera2 from "./Bandera2.png";
+import mochila1 from "./Mochila1.jpeg";
+import mochila2 from "./Mochila2.jpeg";
+import ploteado1 from "./Ploteado1.png";
+import remera2 from "./Remera2.png";
+import remeraF1 from "./RemeraF1.png";
+import remeraT1 from "./RemeraT1.png";
+import sobreNosotros1 from "./SobreNosotros1.png";
+import taza1 from "./Taza1.jpeg";
+import taza2 from "./Taza2.jpeg";
+import gorra1 from "./Gorras1.jpeg";
+import banner from "./Banner1.png";
 
 export {
-    bigImg1,
-    bigImg2,
-    bigImg3,
-    footerLogo,
-    headerLogo,
-
-    offer,
-    thumbnailBackground,
-    thumbnailImg1,
-    thumbnailImg2,
-    thumbnailImg3,
-
-    accesorio1,
-    accesorio2,
-    bandera1,
-    bandera2,
-    mochila1,
-    mochila2,
-    ploteado1,
-    remera2,
-    remeraF1,
-    remeraT1,
-    sobreNosotros1,
-    taza1,
-    taza2,
-    gorra1,
-    banner
-}
+  bigImg1,
+  bigImg2,
+  bigImg3,
+  thumbnailImg1,
+  thumbnailImg2,
+  thumbnailImg3,
+  accesorio1,
+  accesorio2,
+  bandera1,
+  bandera2,
+  mochila1,
+  mochila2,
+  ploteado1,
+  remera2,
+  remeraF1,
+  remeraT1,
+  sobreNosotros1,
+  taza1,
+  taza2,
+  gorra1,
+  banner,
+};
